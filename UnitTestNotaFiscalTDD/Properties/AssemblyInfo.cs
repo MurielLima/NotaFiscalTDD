76f1c036e52467c6fbec33b70a85fd8ec10bff38
Muriel Lima <muriellima2@gmail.com>
2019-08-21 21:55:07 -0300

    Classes de teste

---
+++ UnitTestNotaFiscalTDD/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestNotaFiscalTDD")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTestNotaFiscalTDD")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("4157c989-d882-403d-a8cb-62286221386e")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ UnitTestNotaFiscalTDD/UnitTest.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NotaFiscalTDD.Controller;
+using NotaFiscalTDD.Model;
+
+namespace UnitTestNotaFiscalTDD
+{
+    [TestClass]
+    public class UnitTest
+    {
+        [TestMethod]
+        public void TestGeradorNotaFiscal1()
+        {
+            Fatura fat = new Fatura("Muriel josé Lima","Rua dos bobos nº0",1,Fatura.TipoServico.Consultoria);
+            NotaFiscal nF = GeradorNotaFiscal.GeraNotaFiscal(fat);
+            Assert.AreNotEqual(1,nF.Imposto);
+        }
+        [TestMethod]
+        public void TestGeradorNotaFiscal2()
+        {
+            Fatura fat = new Fatura("Muriel josé Lima", "Rua dos bobos nº0", 1000, Fatura.TipoServico.Consultoria);
+            NotaFiscal nF = GeradorNotaFiscal.GeraNotaFiscal(fat);
+            Assert.AreEqual(750, nF.Imposto);
+        }
+        [TestMethod]
+        public void TestGeradorNotaFiscal3()
+        {
+            Fatura fat = new Fatura("Muriel josé Lima", "Rua dos bobos nº0", 1000, Fatura.TipoServico.Treinamento);
+            NotaFiscal nF = GeradorNotaFiscal.GeraNotaFiscal(fat);
+            Assert.AreEqual(850, nF.Imposto);
+        }
+        [TestMethod]
+        public void TestGeradorNotaFiscal4()
+        {
+            Fatura fat = new Fatura("Muriel josé Lima", "Rua dos bobos nº0", 1000, Fatura.TipoServico.Outros);
+            NotaFiscal nF = GeradorNotaFiscal.GeraNotaFiscal(fat);
+            Assert.AreEqual(940, nF.Imposto);
+        }
+        [TestMethod]
+        public void TestGeradorNotaFiscal5()
+        {
+            Fatura fat = new Fatura("Muriel josé Lima", "Rua dos bobos nº0", 1000, Fatura.TipoServico.Outros);
+            NotaFiscal nF = GeradorNotaFiscal.GeraNotaFiscal(fat);
+            Assert.AreEqual(940, nF.Imposto);
+            Assert.AreEqual("Email enviado com sucesso!", GeradorNotaFiscal.EnviaPorEmail(nF));
+        }
+        [TestMethod]
+        public void TestGeradorNotaFiscal6()
+        {
+            Fatura fat = new Fatura("Muriel josé Lima", "Rua dos bobos nº0", 1000, Fatura.TipoServico.Outros);
+            NotaFiscal nF = GeradorNotaFiscal.GeraNotaFiscal(fat);
+            Assert.AreEqual(940, nF.Imposto);
+            Assert.AreEqual("Salvo com sucesso!", GeradorNotaFiscal.Salvar(nF));
+        }
+        [TestMethod]
+        public void TestGeradorNotaFiscal7()
+        {
+            Fatura fat = new Fatura("Muriel josé Lima", "Rua dos bobos nº0", 1000, Fatura.TipoServico.Outros);
+            NotaFiscal nF = GeradorNotaFiscal.GeraNotaFiscal(fat);
+            Assert.AreEqual(940, nF.Imposto);
+            Assert.AreEqual("Enviado com sucesso!", GeradorNotaFiscal.EnviaParaSistema(nF));
+        }
+    }
+}
